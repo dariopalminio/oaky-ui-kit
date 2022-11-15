4d4b1bf5524e856867a184b6abea37de4d6fd2ca
Dario Palminio <dario.palminio@gmail.com>
2022-11-15 19:27:07 -0300

    change: background color in hover

--- lib/ui-kit/theme/themes.tsx
+++ lib/ui-kit/theme/themes.tsx
@@ -25,7 +25,7 @@ const PrimaryTheme = {
     shadow:'#aaa',
     background:"linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     hover:"black",
-    backgroundHover:"#E5E5E5"
+    backgroundHover:"#CFE0EE"
   }
 };
 
